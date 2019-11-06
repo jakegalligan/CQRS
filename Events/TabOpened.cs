3fb11ab75e292ccac085a2bfdbb620995e160812
jakegalligan <galligan1996@gmail.com>
2019-11-06 14:33:02 -0500

    Create initial repo and add class for tab opened event

---
+++ Events/TabOpened.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQRSTutorial.Events
+{
+    class TabOpened
+    {
+        public Guid Id;
+        public int TableNumber;
+        public string Waiter;
+    }
+}
